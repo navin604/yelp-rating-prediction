4c77347654562a012ce1b70688309589c66a2632
Navin Parmar <navinparmar98@gmail.com>
2023-04-04 00:33:09 -0700

    Added data cleaning for probabilistic model

--- sentiment.py
+++ sentiment.py
@@ -2,15 +2,41 @@ import pandas as pd
 import sys
 import nltk
 from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import CountVectorizer
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
+from typing import List
+import string
+
 
 techniques = ["p"]
+stop = stopwords.words('english')
+
 
 def main():
     file, technique, model = process_args(sys.argv[1:])
     validate_args(technique)
     data = preprocess(file)
-    print(data)
+
+    data = data.head(10000)
+    if technique == "p":
+        probabilistic(data)
+
+
+def probabilistic(data):
+    # Tokenize review
+    data['text'] = data['text'].apply(word_tokenize)
+    # Remove stop words and punctuation
+    data['text'] = data['text'].apply(lambda words: [word for word in words if word not in stop])
+    data['text'] = data['text'].apply(lambda words: [word for word in words if word not in string.punctuation])
+
+
+
+
+def clean_data(data):
+    """ Can be implemented if our models share certain data cleaning methods
+        My model uses tokenization, removal of stop words, removal of punctuation
+    """
+    pass
 
 
 def validate_args(technique):
@@ -18,13 +44,13 @@ def validate_args(technique):
         sys.exit("Invalid classification method")
 
 
-def process_args(args):
+def process_args(args) -> List[str]:
     if len(args) == 2:
         args.append(None)
     return args[0], args[1], args[2],
 
 
-def preprocess(file_name):
+def preprocess(file_name) -> pd.DataFrame:
     json_reader = pd.read_json(file_name, orient='records', lines=True, chunksize=1000000)
     dataSet = next(json_reader)
     while True:
